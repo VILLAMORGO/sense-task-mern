de2c7923e38f395e5881d87aed2251c85412cc63
VILLAMORGO <villamorgo@gmail.com>
2022-08-08 23:05:14 +0800

    Configure package.json to restart dynos

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,10 @@
 const path = require('path')
-
 const express = require('express');
+const colors = require('colors')
 const dotenv = require('dotenv').config();
 const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 var cors = require('cors');
-const { response } = require('express');
 
 const port = process.env.PORT || 5000;
 
@@ -22,19 +21,18 @@ app.use('/api/todos', require('./routes/todoRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
 
 // Serve frontend
-
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
-  
-    app.get('*', (req, res) =>
-      res.sendFile(
-        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
-      )
-    );
-  } else {
-    app.get('/', (req, res) => res.send('Please set to production'));
-  }
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-app.use(errorHandler)
+  app.get('*', (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    )
+  );
+} else {
+  app.get('/', (req, res) => res.send('Please set to production'));
+}
 
-app.listen(port, () => console.log('listening on port ' + port));
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
