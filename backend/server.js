b3ad7e11414010d58a9c903f115a5d83bac2ef04
VILLAMORGO <villamorgo@gmail.com>
2022-08-08 16:09:17 +0800

    Prepare deploy

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
+const path = require('path')
+
 const express = require('express');
 const dotenv = require('dotenv').config();
 const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 var cors = require('cors');
+const { response } = require('express');
 
 const port = process.env.PORT || 5000;
 
@@ -17,6 +20,21 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use('/api/todos', require('./routes/todoRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
+
+// Serve frontend
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+    app.get('*', (req, res) => 
+        res.sendFile(
+            path.resolve(__dirname, '../', 'frontend', 
+            'build', 'index.html')
+        )
+    );
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'))
+}
 
 app.use(errorHandler)
 
